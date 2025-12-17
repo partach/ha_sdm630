9b09781cfdf45fc33674cda1147b7595d25d48b0
partach <partach@gmail.com>
2025-12-17 21:56:00 +0100

    Update options_flow.py

--- custom_components/HA-SDM630/options_flow.py
+++ custom_components/HA-SDM630/options_flow.py
@@ -2,6 +2,7 @@ from homeassistant.components import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import selector
 from homeassistant.components import config_entries
+import voluptuous as vol
 from .const import CONF_REGISTER_SET, REGISTER_SET_BASIC, REGISTER_SET_BASIC_PLUS, REGISTER_SET_FULL, DEFAULT_REGISTER_SET
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
